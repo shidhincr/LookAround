6ebc08ec49ccfb85b629fa6edb54e123b7c3320d
Shidhin <shidhin.cr@flipkart.com>
2014-01-24 18:19:36 +0530

    Setting the list element into view when map marker is clicked

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,103 +1,110 @@
+/*globals angular*/
 "use strict";
 
 
-angular.module("lookAroundApp.controllers",[])
+angular.module( "lookAroundApp.controllers", [ ] )
 
-	.controller( "ZipCodeFrmCtrl", function( $scope, $location, $routeParams ){
-		var placeurl = $routeParams.place || '';
-		$scope.sendZip = function( zipcode ) {
-			$location.path("/search/" + zipcode + "/" + placeurl);
-		}
-	})
+.controller( "ZipCodeFrmCtrl", function( $scope, $location, $routeParams ) {
+  var placeurl = $routeParams.place || "";
+  $scope.sendZip = function( zipcode ) {
+    $location.path( "/search/" + zipcode + "/" + placeurl );
+  };
+} )
 
-	.controller( "SearchCtrl", function( $scope, $routeParams, $location, googleMap, $http ){
-		$scope.zipCode = $routeParams.zipcode;
-		$scope.place = $routeParams.place;
-		if(!$scope.zipCode){
-			$location.path("/");
-		}
-		$http.get("data/places.json").success( function( results ){
-			$scope.places = results.data;
-		});
-		$scope.getUrl = function( placeurl ) {
-			return "#/search/" + $scope.zipCode + placeurl ;
-		};
-		$scope.activeClass = function( place ) {
-			return place.url.slice(1).toLowerCase() === $scope.place ? 'active':'';
-		}
-		if(!$scope.place){
-			$location.path( $scope.getUrl("/atm").slice(1) );
-		}
-		else {
-			googleMap.getGeoCoder().geocode( { address: $scope.zipCode }, function( results, status ) {
-				var lat = results[0].geometry.location.lat(),
-		        	lng = results[0].geometry.location.lng();
+.controller( "SearchCtrl", function( $scope, $routeParams, $location, googleMap, $http ) {
+  $scope.zipCode = $routeParams.zipcode;
+  $scope.place = $routeParams.place;
+  if ( !$scope.zipCode ) {
+    $location.path( "/" );
+  }
+  $http.get( "data/places.json" ).success( function( results ) {
+    $scope.places = results.data;
+  } );
+  $scope.getUrl = function( placeurl ) {
+    return "#/search/" + $scope.zipCode + placeurl;
+  };
+  $scope.activeClass = function( place ) {
+    return place.url.slice( 1 ).toLowerCase( ) === $scope.place ? "active" : "";
+  };
+  if ( !$scope.place ) {
+    $location.path( $scope.getUrl( "/atm" ).slice( 1 ) );
+  } else {
+    googleMap.getGeoCoder( ).geocode( {
+      address: $scope.zipCode
+    }, function( results, status ) {
+      var lat = results[ 0 ].geometry.location.lat( ),
+        lng = results[ 0 ].geometry.location.lng( );
 
-		       	googleMap.placeService.textSearch( {
-		       		query: $scope.place,
-		       		type: $scope.place,
-		       		location: new googleMap._maps.LatLng( lat, lng ),
-		       		radius: 50
-		       	}, function( data ) {
-		       		$scope.$apply(function(){
-		       			$scope.data = data;
-		       		});
-		       	});
-			});
-		}
-		
-	})
-	.controller( "ResultsTabCtrl", function( $scope, $routeParams, $location, googleMap ){
-		$scope.tabs = {
-			"map" : false,
-			"list": true
-		};
-		$scope.selectedMarker = 0;
-		$scope.listView = function(){
-			$scope.tabs = {
-				"map" : false,
-				"list": true
-			};
-		};
-		$scope.mapView = function(){
-			$scope.tabs = {
-				"map" : true,
-				"list": false
-			};
-		};
-		$scope.$watch(function(){
-			return googleMap.selectedMarkerIdx;
-		}, function(newVal){
-			var fn = function(){
-				$scope.selectedMarker = newVal;
-				if( newVal !== null ) {
-					$scope.listView();
-				}
-			}
-			fn();
-		});
-	})
-	.controller( "MainCtrl", function( $scope, $routeParams, $location, $window ){
-		$scope.applied = function(){
-			return !!$routeParams.zipcode;
-		}
-		$scope.$on('$viewContentLoaded', function(event) {
-			$window.ga('send', 'pageview', {'page': $location.path()});
-		});
-	})
-	.controller( "AboutDialogCtrl", function($scope,$window){
-		$scope.opened = false;
-		$scope.open = function () {
-			$scope.opened = true;
-			$window.ga('send', 'pageview', {'page': 'about.html'});
-		};
+      googleMap.placeService.textSearch( {
+        query: $scope.place,
+        type: $scope.place,
+        location: new googleMap._maps.LatLng( lat, lng ),
+        radius: 50
+      }, function( data ) {
+        $scope.$apply( function( ) {
+          $scope.data = data;
+        } );
+      } );
+    } );
+  }
 
-		$scope.close = function () {
-			$scope.opened = false;
-		};
+} )
+  .controller( "ResultsTabCtrl", function( $scope, $routeParams, $location, googleMap, scrollToElem ) {
+    $scope.tabs = {
+      "map": false,
+      "list": true
+    };
+    $scope.selectedMarker = 0;
+    $scope.listView = function( ) {
+      $scope.tabs = {
+        "map": false,
+        "list": true
+      };
+    };
+    $scope.mapView = function( ) {
+      $scope.tabs = {
+        "map": true,
+        "list": false
+      };
+    };
+    $scope.$watch( function( ) {
+      return googleMap.selectedMarkerIdx;
+    }, function( newVal ) {
+      var fn = function( ) {
+        $scope.selectedMarker = newVal;
+        if ( newVal !== null ) {
+          $scope.listView( );
+          scrollToElem.scrollTo( "listItem" + newVal );
+        }
+      };
+      fn( );
+    } );
+  } )
+  .controller( "MainCtrl", function( $scope, $routeParams, $location, $window ) {
+    $scope.applied = function( ) {
+      return !!$routeParams.zipcode;
+    };
+    $scope.$on( "$viewContentLoaded", function( event ) {
+      $window.ga( "send", "pageview", {
+        "page": $location.path( )
+      } );
+    } );
+  } )
+  .controller( "AboutDialogCtrl", function( $scope, $window ) {
+    $scope.opened = false;
+    $scope.open = function( ) {
+      $scope.opened = true;
+      $window.ga( "send", "pageview", {
+        "page": "about.html"
+      } );
+    };
 
-		$scope.opts = {
-			backdropFade: true,
-			dialogFade:true
-		};
-	});
+    $scope.close = function( ) {
+      $scope.opened = false;
+    };
+
+    $scope.opts = {
+      backdropFade: true,
+      dialogFade: true
+    };
+  } );
