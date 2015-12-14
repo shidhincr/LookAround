53837ea25cbc5033957e15c8b748d4d38866457d
Anuja Joshi <joshi_anuja@rocketmail.com>
2015-12-14 13:11:43 +0530

    refs #12 upgraded to AngularJS v1.4.8

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,37 +8,40 @@ angular.module("lookAroundApp.controllers", [ ])
      * [ZipCodeFrmCtrl]
      * Responsible for updating the route whenever zipcode is changed
      * appears in the first page,  and the header
+     *
+     * - default placeurl in route parameters is 'atm'
      * 
      * @param  {[type]} $scope
      * @param  {[type]} $location
      * @param  {[type]} $routeParams
      * @return {[type]}
      */
-    .controller("ZipCodeFrmCtrl", function ($scope, $location, $routeParams) {
-        var placeurl = $routeParams.place || "";
-        $scope.sendZip = function (zipcode) {
-            $location.path("/search/" + zipcode + "/" + placeurl);
-        };
-        
-        // Currently displaying Result near ...
-        // Display filtered item(like Atm, Bank, Zoo..)  instead of static string "Result"
-        $scope.resultTitle = function(place) {
-            var  placeTitle = "";
-            angular.forEach($scope.places, function(result){
-                if(result.url === "/" + place){
-                    placeTitle = result.title;
-                }
-            });
-            return placeTitle;
+    .controller("ZipCodeFrmCtrl", ['$scope', '$location', '$routeParams',
+        function ($scope, $location, $routeParams) {
+            var placeurl = $routeParams.place || "atm";
+            $scope.sendZip = function (zipcode) {
+                $location.path("/search/" + zipcode + "/" + placeurl);
+            };
+
+            // Currently displaying Result near ...
+            // Display filtered item(like Atm, Bank, Zoo..)  instead of static string "Result"
+            $scope.resultTitle = function(place) {
+                var  placeTitle = "";
+                angular.forEach($scope.places, function(result){
+                    if(result.url === "/" + place){
+                        placeTitle = result.title;
+                    }
+                });
+                return placeTitle;
+            }
         }
-    })
+    ])
 
     /**
      * [SearchCtrl]
      * This controller is responsible for the main search in the view
      * 
      * - It first gets the zip code from the route params and check for the place data
-     * - default place type is 'atm'
      * - it then gets the map latitude and longitude from the zipcode ( using Google GeoCoder )
      * - using the places Api, it will fetch the data for the given place type ( 'atm|bar|bus-station' ..etc)
      * - and renders the map using this data
@@ -51,97 +54,96 @@ angular.module("lookAroundApp.controllers", [ ])
      * @param  {[type]} $filter
      * @return {[type]}
      */
-    .controller("SearchCtrl", function ($scope, $routeParams, $location, googleMap, $http, $filter) {
-        $scope.zipCode = $routeParams.zipcode;
-        $scope.place = $routeParams.place;
-        
-        /* redirect to the home page if there's no zipcode */
-        if (!$scope.zipCode) {
-            $location.path("/");
-        }
+    .controller("SearchCtrl",
+        ['$scope', '$routeParams', '$location', 'googleMap', '$http', '$filter',
+            function ($scope, $routeParams, $location, googleMap, $http, $filter) {
+                $scope.zipCode = $routeParams.zipcode;
+                $scope.place = $routeParams.place;
 
-        /* 
-        Gets the default place types
-        @TODO: this should go to a resolve object
-        */
-        $http.get("data/places.json").success(function (results) {
-            $scope.places = results.data;
-        });
-        
-        /**
-         * [getUrl get the url for different type of places ]
-         * @param  {string} placeurl
-         * @return {string}
-         */
-        $scope.getUrl = function (placeurl) {
-            return "#/search/" + $scope.zipCode + placeurl;
-        };
-        
-        /*
-        Simple utility method to set the active class
-         */
-        $scope.activeClass = function (place) {
-            return place.url.slice(1).toLowerCase() === $scope.place ? "active" : "";
-        };
-        
-        /**
-         * Method for finding out the place name for the search results
-         * Used to show in the header
-         * 
-         * @param  {[type]} details
-         * @return {[type]}
-         */
-        $scope.getLocation = function (details) {
-            var location = ( details && details.geometry && details.geometry.location ),
-                out = [ ];
-            if (!location) {
-                return "location not available";
-            } else {
-                angular.forEach(location, function (value, key) {
-                    this.push($filter("number")(value, 4));
-                }, out);
-                return out.join(", ");
-            }
-        };
+                /* redirect to the home page if there's no zipcode */
+                if (!$scope.zipCode) {
+                    $location.path("/");
+                }
 
-        if (!$scope.place) {
-            /* select the default place type as 'atm' */
-            $location.path($scope.getUrl("/atm").slice(1));
-        } else {
-            /* 
-            start the Geocoding to get the latitude and longitude from the 
-            zipcode proviced. This lat/long will be served to the places api to fetch the places details
-            */
-            googleMap.getGeoCoder().geocode({
-                address: $scope.zipCode
-            }, function (results, status) {
-                var lat = results[ 0 ].geometry.location.lat(),
-                    lng = results[ 0 ].geometry.location.lng();
-
-                /* $scope.$apply is required as this function will be executed inside the GeoCoder context */
-                $scope.$apply(function () {
-                    $scope.searchplace = results[ 0 ] && results[ 0 ].formatted_address;
+                /*
+                Gets the default place types
+                @TODO: this should go to a resolve object
+                */
+                $http.get("data/places.json").success(function (results) {
+                    $scope.places = results.data;
                 });
 
-                /* Do a text search and find all the places for the given query ( place type ) */
-                googleMap.placeService.textSearch({
-                    query: $scope.place,
-                    type: $scope.place,
-                    location: new googleMap._maps.LatLng(lat, lng),
-                    radius: 50
-                }, function (data) {
-                    /* 
-                    Once getting the data, set it to the controller scope.
-                    $scope.$apply is required because this function will be executed in the googleMap object scope
-                    */
+                /**
+                 * [getUrl get the url for different type of places ]
+                 * @param  {string} placeurl
+                 * @return {string}
+                 */
+                $scope.getUrl = function (placeurl) {
+                    return "#/search/" + $scope.zipCode + placeurl;
+                };
+
+                /*
+                Simple utility method to set the active class
+                 */
+                $scope.activeClass = function (place) {
+                    return place.url.slice(1).toLowerCase() === $scope.place ? "active" : "";
+                };
+
+                /**
+                 * Method for finding out the place name for the search results
+                 * Used to show in the header
+                 *
+                 * @param  {[type]} details
+                 * @return {[type]}
+                 */
+                $scope.getLocation = function (details) {
+                    var location = ( details && details.geometry && details.geometry.location ),
+                        out = [ ];
+                    if (!location) {
+                        return "location not available";
+                    } else {
+                        angular.forEach(location, function (value, key) {
+                            this.push($filter("number")(value, 4));
+                        }, out);
+                        return out.join(", ");
+                    }
+                };
+
+
+                /*
+                start the Geocoding to get the latitude and longitude from the
+                zipcode proviced. This lat/long will be served to the places api to fetch the places details
+                */
+                googleMap.getGeoCoder().geocode({
+                    address: $scope.zipCode
+                }, function (results, status) {
+                    var lat = results[ 0 ].geometry.location.lat(),
+                        lng = results[ 0 ].geometry.location.lng();
+
+                    /* $scope.$apply is required as this function will be executed inside the GeoCoder context */
                     $scope.$apply(function () {
-                        $scope.data = data;
+                        $scope.searchplace = results[ 0 ] && results[ 0 ].formatted_address;
+                    });
+
+                    /* Do a text search and find all the places for the given query ( place type ) */
+                    googleMap.placeService.textSearch({
+                        query: $scope.place,
+                        type: $scope.place,
+                        location: new googleMap._maps.LatLng(lat, lng),
+                        radius: 50
+                    }, function (data) {
+                        /*
+                        Once getting the data, set it to the controller scope.
+                        $scope.$apply is required because this function will be executed in the googleMap object scope
+                        */
+                        $scope.$apply(function () {
+                            $scope.data = data;
+                        });
                     });
                 });
-            });
-        }
-
-    })
+            }
+        ]
+    )
     
     /**
      * [ResultsTabCtrl]
@@ -154,56 +156,60 @@ angular.module("lookAroundApp.controllers", [ ])
      * @param  {[type]} scrollToElem
      * @return {[type]}
      */
-    .controller("ResultsTabCtrl", function ($scope, $routeParams, $location, googleMap, scrollToElem) {
-        $scope.tabs = {
-            "map": false,
-            "list": true
-        };
-        $scope.selectedMarker = 0;
+    .controller("ResultsTabCtrl",
+        ['$scope', '$routeParams', '$location', 'googleMap', 'scrollToElem',
+            function ($scope, $routeParams, $location, googleMap, scrollToElem) {
+                $scope.tabs = {
+                    "map": false,
+                    "list": true
+                };
+                $scope.selectedMarker = 0;
 
-        $scope.listView = function () {
-            $scope.tabs = {
-                "map": false,
-                "list": true
-            };
-        };
+                $scope.listView = function () {
+                    $scope.tabs = {
+                        "map": false,
+                        "list": true
+                    };
+                };
 
-        $scope.mapView = function () {
-            $scope.tabs = {
-                "map": true,
-                "list": false
-            };
-        };
+                $scope.mapView = function () {
+                    $scope.tabs = {
+                        "map": true,
+                        "list": false
+                    };
+                };
 
-        $scope.selectFromList = function(num) {
-            $scope.mapView();
-            $scope.selectedMarker = num;
-            // need to get better user experience
-            googleMap.zoomToMarker(num);
-            googleMap.bounceMarker(num);
-        }
-
-        /*
-        Watches for any click inside the map markers, and switches to the list view.
-        Also scroll down the details of the selected marker in the list.
-         */
-        $scope.$watch(function () {
-            return googleMap.selectedMarkerIdx;
-        }, function (newVal) {
-            var fn = function () {
-                $scope.selectedMarker = newVal;
-                if (newVal !== null) {
-                    $scope.listView();
+                $scope.selectFromList = function(num) {
+                    $scope.mapView();
+                    $scope.selectedMarker = num;
                     // need to get better user experience
-                    googleMap.zoomToMarker(newVal);
-                    googleMap.bounceMarker(newVal);
-                    scrollToElem.scrollTo("listItem" + newVal);
+                    googleMap.zoomToMarker(num);
+                    googleMap.bounceMarker(num);
                 }
-            };
-            fn();
-        });
-    })
-    
+
+                /*
+                Watches for any click inside the map markers, and switches to the list view.
+                Also scroll down the details of the selected marker in the list.
+                 */
+                $scope.$watch(function () {
+                    return googleMap.selectedMarkerIdx;
+                }, function (newVal) {
+                    var fn = function () {
+                        $scope.selectedMarker = newVal;
+                        if (newVal !== null) {
+                            $scope.listView();
+                            // need to get better user experience
+                            googleMap.zoomToMarker(newVal);
+                            googleMap.bounceMarker(newVal);
+                            scrollToElem.scrollTo("listItem" + newVal);
+                        }
+                    };
+                    fn();
+                });
+            }
+        ]
+    )
+
     /**
      * [MainCtrl]
      * @param  {[type]} $scope
@@ -212,21 +218,23 @@ angular.module("lookAroundApp.controllers", [ ])
      * @param  {[type]} $window
      * @return {[type]}
      */
-    .controller("MainCtrl", function ($scope, $routeParams, $location, $window) {
-        // checks if the url contains any valid zipcode
-        $scope.applied = function () {
-            return !!$routeParams.zipcode;
-        };
-        // some Google analytics
-        $scope.$on("$viewContentLoaded", function (event) {
-            $window.ga("send", "pageview", {
-                "page": $location.path()
+    .controller("MainCtrl", ['$scope', '$routeParams', '$location', '$window',
+        function ($scope, $routeParams, $location, $window) {
+            // checks if the url contains any valid zipcode
+            $scope.applied = function () {
+                return !!$routeParams.zipcode;
+            };
+            // some Google analytics
+            $scope.$on("$viewContentLoaded", function (event) {
+                $window.ga("send", "pageview", {
+                    "page": $location.path()
+                });
             });
-        });
-    })
+        }
+    ])
 
     // Just shows something about me and how I did this.
-    .controller("AboutDialogCtrl", function ($scope, $window) {
+    .controller("AboutDialogCtrl", ['$scope', '$window', function ($scope, $window) {
         $scope.opened = false;
         $scope.open = function () {
             $scope.opened = true;
@@ -244,4 +252,4 @@ angular.module("lookAroundApp.controllers", [ ])
             backdropFade: true,
             dialogFade: true
         };
-    });
+    }]);
